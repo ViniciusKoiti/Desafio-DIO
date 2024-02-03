b5c649513aa1ca6b0775f8c2fc9cf2512d69cb3c
Vinicius Koiti <viniciusnakahara@gmail.com>
2024-02-03 15:29:39 -0300

    Alterações de desafio-reactor

---
+++ src/main/java/br/com/dio/desafioreactor/Question2.java
@@ -0,0 +1,17 @@
+package br.com.dio.desafioreactor;
+
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class Question2 {
+
+    /*
+    Recebe uma lista de usuários e retorna a quantos usuários admin tem na lista
+     */
+    public Mono<Long> countAdmins(final List<User> users){
+        Long quantidadeAdms = users.stream().filter(User::isAdmin).count();
+        return Mono.justOrEmpty(quantidadeAdms);
+    }
+
+}
